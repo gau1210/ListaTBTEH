78bb665fb0624231af543dcdf2c2b4adba421487
gau1210 <gau1210@gmail.com>
2017-04-02 16:28:36 -0300

    Add files via upload

---
+++ Menor de três.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int menor_de_tres(int a, int b, int c){
+    
+    if((a<=b)&&(a<=c)){
+        
+        printf("o valor de A e maior %d", a);
+    }
+        
+        if(b<=c){
+        
+        printf("o valor de B e maior %d", b);
+    }else{
+        
+        printf("o valor de C e maior %d", c);
+    }
+
+
+}
+    int main(){
+        
+        int a,b,c;
+        
+        printf("digite um valor");
+        scanf("%d",&a);
+        printf("digite um valor");
+        scanf("%d",&b);
+        printf("digite um valor");
+        scanf("%d",&c);
+        
+        
+        menor_de_tres(a,b,c);
+        return 0;
+        
+}
