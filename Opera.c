d17c0972ea5fbe0472fd0294f9662efa506d8a49
gau1210 <gau1210@gmail.com>
2017-04-02 14:06:09 -0300

    Add files via upload

---
+++ Opera.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float opera ( float a, float b, int c){
+    
+    float result;
+    
+    if (c==0){
+        
+       result= a+b;
+       return result;
+        
+    }
+        if (c==1){
+            
+        result= b-a;
+        return result;
+            
+        }
+        
+        if (c==2){
+            
+        result= a/b;
+        return result;
+            
+        }
+        if(c==3){
+            
+         result= a*b;
+         return result;
+       
+        }
+  
+    }
+    
+    
+int main(){
+    
+    float a=3,b=2, result;
+    int c;
+    
+    printf("digite um valor");
+    scanf("%d",&c);
+    
+    opera(a,b,result);
+    printf("%d",result);
+      
+    return 0;
+    
+}
+            
+            
