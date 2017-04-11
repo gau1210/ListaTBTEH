bdec4df0a03c021013991547a861021cb142a04f
gau1210 <gau1210@gmail.com>
2017-04-10 22:00:14 -0300

    Update Divisores.c

--- Divisores.c
+++ Divisores.c
@@ -5,34 +5,35 @@
 void divisores(int a, int b[])
 {
 
-	int i,num=0;
-	int div;
+int i,num=0;
+int div;
 	
-	if(a>0){
-        
-		for(i=1;i<=a;i++){
-            
-            div= a%i;
-            
+if(a>0){
+
+	for(i=1;i<=a;i++){
+
+		div= a%i;
+
 		if(div== 0){
-                
-		b[num]=i;
-		num++;
-				
-	printf("O valor e:d%\n",num);
-				
-            }
-        }
-    }
+
+			b[num]=i;
+			num++;
+
+			printf("O valor e:%d\n",i);
+
+			}
+		}
+	}
 }
 
 int main()
 {
-    int a;
-    
-    printf("digite um valor");
-    scanf("%d",a);
-    
-    //divisores(a,num); Duvida
-    return 0;
+	int a;
+	int div[100];
+
+	printf("digite um valor");
+	scanf("%d",&a); 
+
+	divisores(a,div);
+	return 0;
 }
